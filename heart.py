72aefe4ad5860eadbda3e2d5f5df5ca9c7de7626
Akash Kumar <akashkumar9576386319@gmail.com>
2025-10-05 23:15:14 +0530

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,14 @@
+import turtle as t 
+t.hideturtle()
+t.begin_fill()
+t.fillcolor('red')
+t.left(140)
+t.forward(180)
+t.circle(-80,200)
+t.setheading(60)
+t.circle(-80,200)
+t.forward(180)
+t.end_fill()
+
+
+t.done()
